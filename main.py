2ee48530c39489c7efd9c8d8d8b835b10a4ae54d
Franco Talarico <francotalarico01@gmail.com>
2024-04-19 11:05:10 -0300

    creando Label

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from tkinter import *
 
 ventana = ttk.Tk()
 ventana.title("Pañol Herramientas")
+ventana.geometry("500x500")
 
+lbl_herramientas = ttk.Label(ventana, text="Hola", font=("helvetica", 15))
+lbl_herramientas.pack()
 
 
 ventana.mainloop()
